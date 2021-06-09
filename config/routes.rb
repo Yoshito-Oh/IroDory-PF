69b513f8d704874300622a327ae2e051c7d165bb
Yoshito-Oh <true.lier44@gmail.com>
2021-06-09 01:42:11 +0000

    【21/06/08】top, deviseの導入とviewページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'homes#top'
+
+  #管理者アクション=====
+  #namespace :admin do
+
+  #end
+  #=====================
+
+  #ログイン時可能のアクション==
+  #namespace :public do
+  #end
+  #======================================
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
