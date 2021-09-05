1bb034dc8b5b7c79438025cb1a6e41bb58be1e32
Yoshito-Oh <true.lier44@gmail.com>
2021-09-05 12:39:22 +0000

    [210905]controllerの整理

--- app/controllers/public/item_images_controller.rb
+++ app/controllers/public/item_images_controller.rb
@@ -1,8 +1,10 @@
 class Public::ItemImagesController < ApplicationController
+  #一覧表示ページ===============================================
   def index
     @item_images = ItemImage.page(params[:page]).reverse_order
   end
-
+  #=============================================================
+  #詳細ページ==========================================================================
   def show
     @item_image = ItemImage.find(params[:id])
     @post_images = PostImage.where(status: true).where(item_image_id: @item_image.id)
@@ -10,7 +12,8 @@ class Public::ItemImagesController < ApplicationController
       @current_user_post_image = PostImage.where(status: true).where(user_id: current_user.id)
     end
   end
-
+  #====================================================================================
+  #素材をダウンロードするためのメソッド================================================
   def download
     download_file = ItemImage.find(params[:item_image_id])
     attacher = download_file.send(:"#{:image}_attacher")
@@ -19,4 +22,5 @@ class Public::ItemImagesController < ApplicationController
     #/attachments/f1081e9aef0a07d35d292c58a0da4b21527ee2f0/store/6dcf4f1883b227943a788ac3071529a6aedffcf8a6947704484eaa6e549d/image.png
     #/attachments/aab5c29bd5b8c5fd4bb3238228c5cbbedfbdd906/store/6dcf4f1883b227943a788ac3071529a6aedffcf8a6947704484eaa6e549d/image.png
   end
+  #====================================================================================
 end
